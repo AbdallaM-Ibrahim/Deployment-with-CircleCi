021e6b1d383f9f117e9672c4d5deeacf1a8c2bb1
AbdallaM-Ibrahim <Abdalla.mostafa19200@gmail.com>
2023-02-27 23:09:40 +0200

    fix typo 2

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://udagarm2023/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagarm2023/
+aws s3 cp --recursive --acl public-read ./www s3://udagram2023/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram2023/
